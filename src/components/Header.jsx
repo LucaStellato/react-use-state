16a07804dd3cf14e8f9140baa6e56b9ec5a977aa
LucaStellato <lucastellato435@gmail.com>
2025-11-07 11:39:36 +0100

    import header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Header() {
+    return (
+        <>
+            <div className="--bs-secondary-color">
+                <h1>Learn Web development</h1>
+            </div>
+        </>
+    )
+}
