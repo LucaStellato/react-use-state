981a5c397c7f0b5f59d09583a4b2e20eab3b8349
LucaStellato <lucastellato435@gmail.com>
2025-11-07 11:47:31 +0100

    add title header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@
 export default function Header() {
     return (
         <>
-            <div className="--bs-secondary-color">
-                <h1>Learn Web development</h1>
-            </div>
+            <div>
+                <h1>Learn Web development </h1>
+            </div >
         </>
     )
 }
